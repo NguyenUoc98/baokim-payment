1b4f79fdf7c4344ad0c17d5faebd6a5795dc8dd5
Nguyễn Văn Ước <uocnv.soict.hust@gmail.com>
2023-07-12 14:17:00 +0700

    add momo

---
+++ src/Clients/MOMO.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm
+ * Filename: Momo.php
+ * User: Nguyễn Văn Ước
+ * Date: 11/07/2023
+ * Time: 17:23
+ */
+
+namespace Uocnv\BaokimPayment\Clients;
+
+use Uocnv\BaokimPayment\Enums\PaymentMethod;
+
+class MOMO extends JWTClient
+{
+    public static function request(
+        int $transactionId,
+        int $amount,
+        string $referer,
+        int $bankId = 0,
+        string $userEmail = '',
+        string $userPhone = ''
+    ): ?array {
+        self::$paymentMethod = PaymentMethod::MOMO;
+        return parent::request($transactionId, $amount, $referer, $bankId, $userEmail, $userPhone);
+    }
+
+    public static function checkValidData(array $response): array
+    {
+        self::$paymentMethod = PaymentMethod::MOMO;
+        return parent::checkValidData($response);
+    }
+}
